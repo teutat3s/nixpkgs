7129beb57a8ee50d58dc930bf17c8d729fd67185
Yureka <yureka@cyberchaos.dev>
2026-05-11 19:29:56 +0200

    electron: update.py: fetch missing-hashes
    Co-authored-by: teutat3s <10206665+teutat3s@users.noreply.github.com>

--- pkgs/development/tools/electron/common.nix
+++ pkgs/development/tools/electron/common.nix
@@ -11,6 +11,7 @@
   npmHooks,
   yarn-berry_4,
   unzip,
+  writers,
 
   libnotify,
   libpulseaudio,
@@ -68,10 +69,12 @@ in
 
   npmRoot = "third_party/node";
 
+  missingHashes = lib.optionalString (info.electron_yarn_data ? "missing_hashes") writers.writeJSON "missing-hashes.json" info.electron_yarn_data.missing_hashes;
   yarnOfflineCache = yarn-berry.fetchYarnBerryDeps {
     src = gclientDeps."src/electron".path;
     patches = [ yarnPatch ];
-    hash = info.electron_yarn_hash;
+    hash = info.electron_yarn_data.hash;
+    missingHashes = lib.optionalString (info.electron_yarn_data ? "missing_hashes") writers.writeJSON "missing-hashes.json" info.electron_yarn_data.missing_hashes;
   };
 
   dontYarnBerryInstallDeps = true; # we'll run the hook manually
